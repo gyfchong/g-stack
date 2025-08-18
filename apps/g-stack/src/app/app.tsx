b14d2bb453c2ffaf6734b8cd124a13a9b09f1a3c
geoff <gyfchong@proton.me>
2025-08-18 20:11:38 +1000

    refactor: replace NxWelcome component with a simple header in App component and remove unused NxWelcome file

--- apps/g-stack/src/app/app.tsx
+++ apps/g-stack/src/app/app.tsx
@@ -1,11 +1,9 @@
-import NxWelcome from './nx-welcome';
-
 import { Route, Routes, Link } from 'react-router-dom';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="@g-stack/g-stack" />
+      <h1>G-Stack</h1>
 
       {/* START: routes */}
       {/* These routes and navigation have been generated for you */}
